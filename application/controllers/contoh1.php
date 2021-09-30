10ca36bce7363162d877d35d97fce7ead7c32f27
nurmasita1 <itdoeta910@gmail.com>
2021-09-30 11:07:14 +0700

    menambahkan pustaka-booking

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+    echo "<h1>Perkenalkan Saya Nurmasita Andini</h1>";
+    echo "NIM 19200953 <br>
+        Saya tingga di daerah Tangerang <br>
+        beberapa hal yang saya sukai adalah
+        catur, baca buku, dan nonton anime";
+ }
+}
